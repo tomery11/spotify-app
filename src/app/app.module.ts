938090736cecb87fd79864f04047568f25d03da9
tomer <tomery@treepodia.com>
2021-10-07 15:44:13 +0300

    initial commit - have design for two pages and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlbumCardComponent } from './components/album-card/album-card.component';
+import { DetailsPageComponent } from './components/details-comp/details-page/details-page.component';
+import { TrackCardComponent } from './components/details-comp/track-card/track-card.component';
+import { RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-comp/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumCardComponent,
+    DetailsPageComponent,
+    TrackCardComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomePageComponent},
+      {path: 'albums', component: DetailsPageComponent}
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
